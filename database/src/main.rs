59809726bc669961c862619df90051fc1823db87
Samuel Ogu <hello@samuelogu.com>
2021-09-03 22:52:21 +0100

    write to file

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().expect("Key is required");
+    let value = arguments.next().unwrap();
+    println!("The key is {} and the value is {}", key, value);
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("database.rs", contents);
 }
